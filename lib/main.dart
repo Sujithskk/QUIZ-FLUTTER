241dff6474ebb795cccbffe5c5a6f792b4e2a953
Sujithskk <sujithkumar@theshivgames.com>
2023-10-19 16:37:17 +0530

    fgjhksdgkhdrjlkgzdfnlghuiasogjhmn

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'suji '
-      ;ljkhgfdsghjokpDemo',
+      title: 'suji kkkkkkkDemo',
       theme: ThemeData(
         // This is the theme of your application.
         //
